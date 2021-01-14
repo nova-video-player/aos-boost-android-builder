0e34519f71011eae19c06201ea543d3aaebf91b0
CourvilleSoftware <software@courville.org>
2021-01-14 09:41:32 +0100

    androidSdk/Ndk detect/install new scheme

--- bootstrap_avp_boost.sh
+++ bootstrap_avp_boost.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-for ARCH in arm arm64 x86 x86_64; do
-	case "${ARCH}" in
+for ARCH in arm arm64 x86 x86_64
+do
+  case "${ARCH}" in
     'arm')
-		ABI=armeabi-v7a ;;
+      ABI=armeabi-v7a ;;
     'arm64')
-        ABI=arm64-v8a ;;
+      ABI=arm64-v8a ;;
     *)
-        ABI=${ARCH} ;;
-    esac
-	if [[ ! -d dist-${ABI} ]]; then
-		( . build.sh -a ${ARCH} )
-	fi
-
+      ABI=${ARCH} ;;
+  esac
+  if [[ ! -d dist-${ABI} ]]; then
+    ( . build.sh -a ${ARCH} )
+  fi
 done
+
 true
